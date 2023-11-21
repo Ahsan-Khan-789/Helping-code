61d61fabc35b0910c830c83b1419e4f64a34b446
Ahsan-Khan-789 <134946648+Ahsan-Khan-789@users.noreply.github.com>
2023-11-21 13:11:20 +0500

    Update help-code.php

--- help-code.php
+++ help-code.php
@@ -43,6 +43,10 @@ function alpha() {
  wp_enqueue_style( 'Montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap', false );
  wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
 
+if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+wp_enqueue_script( 'comment-reply' );
+}
+}
 add_action( 'wp_enqueue_scripts', 'alpha' );
 
 /******End enque file *******/
